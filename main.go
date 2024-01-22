d773fb048739c8bdc90fed22af7a14c07a637a38
joba <jobtennis21@gmail.com>
2024-01-22 11:06:30 +0300

    strings

--- main.go
+++ main.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
 
 	fmt.Println("hello world")
+	var name string = "eyob"
+	fmt.Printf("my name is %v nigussie \n", name)
+	var str = fmt.Sprintf("my name is %v nigussie", name)
+	println(str)
+
 }
